f590083c82c86e6a09cca1edf7fd7c1916534e94
artlenk.ru@gmail.com <df5ukoi7ht>
2017-04-29 17:01:12 +0300

    Created view images block. Fixed minor bugs

--- src/js/common.js
+++ src/js/common.js
@@ -1,25 +1,4 @@
-
-
-/*
 $(document).ready(function () {
-    $(".navbar-toggle").click(
-        function () {
-            $('.navbar').toggleClass("navbar-active");
-        }
-    );
-
-    $(".contacts-form-toggle").click(
-        function () {
-            $('.contacts-form-toggle').toggleClass("contacts-form-toggle-open");
-        }
-    );
-
-    $(".search-left-head-arrow").click(
-        function () {
-            $('.search-left').toggleClass("search-left-open search-left-closed");
-        }
-    );
-
     $(function () {
         $("a[href^='#']").click(function () {
             var _href = $(this).attr("href");
@@ -27,73 +6,4 @@ $(document).ready(function () {
             return false;
         });
     });
-
-    $('.select-open').on('click', function () {
-        $(this).toggleClass('select-active');
-        $(this).parent().find('.make-choice').toggleClass('make-choice-active');
-    });
-
-    $('.select-open + .arrow').on('click', function () {
-        $(this).parent().find('.select-open').toggleClass('select-active');
-        $(this).parent().find('.make-choice').toggleClass('make-choice-active');
-    });
-
-    $('.datepicker-here').on('click', function () {
-        $(this).toggleClass('datepicker-here-open');
-    });
-
-    $('.hint-box').on('click', function () {
-        $(this).toggleClass('hint-box-open');
-    });
-
-    var swiper = new Swiper('.partners-carousel', {
-        grabCursor: true,
-        slidesPerView: 5,
-        breakpoints: {
-            1199: {
-                slidesPerView: 4
-            },
-            991: {
-                slidesPerView: 3
-            },
-            767: {
-                slidesPerView: 2
-            },
-            320: {
-                slidesPerView: 1
-            }
-        }
-    });
-
-    var statisticsCarousel = new Swiper('.statistics-carousel', {
-        grabCursor: true,
-        slidesPerView: 2
-    });
 });
-
-var searchLeftContent = $(".search-left-content");
-
-searchLeftContent.mCustomScrollbar({
-    theme: "dark-3"
-});
-
-
-$(".make-choice").mCustomScrollbar({
-    theme: "dark-3"
-});
-
-$(".insurance-drobdown-scroll").mCustomScrollbar({
-    theme: "dark-3"
-});
-
-$('.insurance-type-point').on('click', function () {
-    $('.insurance-type-point').removeClass('current');
-    $(this).addClass('current');
-});
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip({
-        container: 'body'
-    })
-});
-*/
